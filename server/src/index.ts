666089a18d0879762f18b327532830b64143dddd
karanShaw000 <shawkaran2002@gmail.com>
2025-04-08 02:30:14 +0530

    fix cors error from backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,42 +1,55 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import errorHandler from "./middlewares/errorHandler"
-import { userRouter } from "./routes/user"
-import { blogRouter } from "./routes/blog"
-import connectDB from "./db/dbConnect"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import cors, { CorsOptions } from "cors";
+import errorHandler from "./middlewares/errorHandler";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
+import connectDB from "./db/dbConnect";
+import cookieParser from "cookie-parser";
 
+dotenv.config();
+const port = process.env.PORT || 5000;
+const cookieSecret = process.env.COOKIE_SECRET as string;
+const fronendUrl = process.env.FRONTEND_URL as string;
+const app = express();
 
-dotenv.config()
-const port = process.env.PORT || 5000
-const cookieSecret = process.env.COOKIE_SECRET as string
-const fronendUrl = process.env.FRONTEND_URL as string
-const app = express()
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:4173",
+  fronendUrl,
+];
 
-const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:4173", fronendUrl],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "Set-Cookie",
-    "Access-Control-Allow-Origin",
-  ],
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    const env = process.env.NODE_ENV;
+
+    if (env === "development") {
+      // Allow all in dev mode
+      callback(null, true);
+    } else {
+      // Production: allow only whitelisted domains
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    }
+  },
+  allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   optionsSuccessStatus: 200,
   credentials: true,
 };
 
+app.use(cors(corsOptions));
+app.use(cookieParser(cookieSecret));
+app.use(express.json());
 
-app.use(cors(corsOptions))
-app.use(cookieParser(cookieSecret))
-app.use(express.json())
+connectDB();
+app.use("/api/user", userRouter);
+app.use("/api/blog", blogRouter);
 
-connectDB()
-app.use("/api/user", userRouter)
-app.use("/api/blog", blogRouter)
-
-app.use(errorHandler)
+app.use(errorHandler);
 app.listen(port, () => {
-  console.log(`Server running at port ${port}`)
-})
+  console.log(`Server running at port ${port}`);
+});
